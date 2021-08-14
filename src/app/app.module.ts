58e8a4d5bc7c4d712f33a11b6b53a2c2c90b7a05
sunilgill8731 <gpreetkaur1@gmail.com>
2021-08-14 17:28:46 +0530

    gp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
@@ -23,6 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    
+  ]
 })
 export class AppModule { }
